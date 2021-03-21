aa3d71ff5f2d47db2aaea641df35bfd6a80c160d
aviato-vasile <vasile@aviato.ro>
2021-03-21 12:29:49 +0200

    add sprinta

--- src/Tools.php
+++ src/Tools.php
@@ -1,7 +1,5 @@
 <?php
-
-declare(strict_types=1);
-
+declare(strict_types = 1);
 namespace Avi;
 
 /**
@@ -11,40 +9,55 @@ namespace Avi;
  */
 class Tools
 {
+
 	/**
 	 * Apply default values to the array,
 	 * mainly used for arrays which can't have specific default values defined
+	 *
 	 * @return array with default values
 	 * @param $attributes array
 	 * @param $defaultAttributes array
 	 */
 	public function applyDefault($attributes, $defaultAttributes)
 	{
-		if (!is_array($attributes) || !is_array($defaultAttributes)) {
+		if (! is_array($attributes) || ! is_array($defaultAttributes)) {
 			return false;
 		}
 
 		foreach ($defaultAttributes as $k => $v) {
-			if(!array_key_exists($k, $attributes)) {
+			if (! array_key_exists($k, $attributes)) {
 				$attributes[$k] = $v;
 			}
 		}
-		return($attributes);
+		return ($attributes);
 	}
 
 	/**
+	 *
 	 * @return string Return a string produced according to the pattern by replaceing {*} with array member
 	 * @param string $pattern
 	 * @param array $array
 	 *
-	 * @example
-	 * |	str_suplant('<div id="{id}">{text}</div>', array('id' => 1, 'text' => 'aviato'))
-	 * |	will return: <div id="1">aviato</div>
+	 * @example | str_suplant('<div id="{id}">{text}</div>', array('id' => 1, 'text' => 'aviato'))
+	 *          | will return: <div id="1">aviato</div>
 	 */
-	public function str_supplant($pattern, $array) {
+	public function str_supplant($pattern, $array)
+	{
 		foreach ($array as $k => $v) {
 			$pattern = str_replace(sprintf('{%s}', $k), $v, $pattern);
 		}
 		return $pattern;
+	}
+
+	/**
+	 * Alias of str_supplant easy to remember sprinta
+	 *
+	 * @param string $pattern
+	 * @param array $array
+	 * @return string
+	 */
+	public function sprinta($pattern, $array)
+	{
+		return self::str_supplant($pattern, $array);
 	}
 }
