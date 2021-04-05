7898d6039d3c9343437cadcc5b3461b2e1d88d88
aviato-vasile <vasile@aviato.ro>
2021-04-05 15:20:15 +0300

    testing Avi Ui -> page / meta

--- tests/AviUi-Test.php
+++ tests/AviUi-Test.php
@@ -7,9 +7,9 @@ require_once dirname(__FILE__) . '/assets/Sections.php';
 use PHPUnit\Framework\TestCase;
 use Avi\UI as AviUi;
 
-
 final class testAviatoUi extends TestCase
 {
+
 
 	public function testFn_Construct(): void
 	{
@@ -35,7 +35,7 @@ final class testAviatoUi extends TestCase
 
 	public function testFn_Section(): void
 	{
-		//Object section
+		// Object section
 		$aviUi = new AviUi();
 		$response = $aviUi->Section('test', [], true);
 		$test = '<section id="test" class="sec-obj-test">test section</section>';
@@ -44,28 +44,30 @@ final class testAviatoUi extends TestCase
 
 		$response = $aviUi->Section('test', [
 			'wrapper' => false,
-			'javascript' => ['test.js']
+			'javascript' => [
+				'test.js'
+			]
 		], true);
 		$test = 'test section';
-		//var_dump($response); // <-- uncomment this line to see the result!
+		// var_dump($response); // <-- uncomment this line to see the result!
 		$this->assertEquals($test, $response);
 
 		$test = 'test.js';
-		$this->assertEquals($test, $aviUi -> page['javascript'][0]);
+		$this->assertEquals($test, $aviUi->page['javascript'][0]);
 
-		//Html section
-		$response = $aviUi->Section('test', [
-			'wrapper' => false,
-			'class' => 'test',
-			'type' => 'html',
-			'root' => dirname(__FILE__)
-		], true);
+		// Html section
+		$response = $aviUi->Section('test',
+			[
+				'wrapper' => false,
+				'class' => 'test',
+				'type' => 'html',
+				'root' => dirname(__FILE__)
+			], true);
 		$test = '<div class="html">Test</div>';
-		//var_dump($response); // <-- uncomment this line to see the result!
+		// var_dump($response); // <-- uncomment this line to see the result!
 		$this->assertEquals($test, $response);
 
-
-		//PHP section
+		// PHP section
 		$response = $aviUi->Section('test', [
 			'wrapper' => false,
 			'type' => 'php',
@@ -75,20 +77,19 @@ final class testAviatoUi extends TestCase
 		// var_dump($response); // <-- uncomment this line to see the result!
 		$this->assertEquals($test, $response);
 
-
-		//Missing Html section
-		$response = $aviUi->Section('missing', [
-			'wrapper' => false,
-			'class' => 'test',
-			'type' => 'html',
-			'root' => dirname(__FILE__)
-		], true);
+		// Missing Html section
+		$response = $aviUi->Section('missing',
+			[
+				'wrapper' => false,
+				'class' => 'test',
+				'type' => 'html',
+				'root' => dirname(__FILE__)
+			], true);
 		$test = '';
-		//var_dump($response); // <-- uncomment this line to see the result!
+		// var_dump($response); // <-- uncomment this line to see the result!
 		$this->assertEquals($test, $response);
 
-
-		//Missing PHP section
+		// Missing PHP section
 		$response = $aviUi->Section('missing', [
 			'wrapper' => false,
 			'type' => 'php',
@@ -98,19 +99,21 @@ final class testAviatoUi extends TestCase
 		// var_dump($response); // <-- uncomment this line to see the result!
 		$this->assertEquals($test, $response);
 
-
-		//Missing object
+		// Missing object
 		$aviUi = new AviUi();
-		$response = $aviUi->Section('test', ['wrapper' => false], true);
+		$response = $aviUi->Section('test', [
+			'wrapper' => false
+		], true);
 		$test = 'test section';
 		// var_dump($response); // <-- uncomment this line to see the result!
 		$this->assertEquals($test, $response);
 
-
-		//Test section echo response
+		// Test section echo response
 		ob_start();
 		$aviUi = new AviUi();
-		$aviUi->Section('test', ['wrapper' => false], false);
+		$aviUi->Section('test', [
+			'wrapper' => false
+		], false);
 		$result = ob_get_clean();
 		$test = 'test section';
 		// var_dump($response); // <-- uncomment this line to see the result!
@@ -120,44 +123,78 @@ final class testAviatoUi extends TestCase
 
 	public function testFn_Page(): void
 	{
-		$template = '<!DOCTYPE html>'."\n";
-		$template .= '<html lang="en-EN">'."\n";
-		$template .= '<head><meta charset="UTF-8"><meta http-equiv="content-type" content="text/html"><title>website</title><link rel="shortcut icon" href="//www.aviato.ro/favicon.ico"/>%s</head>'."\n";
-		$template .= '<body%s>'."\n";
-		$template .= '%s'."\n";
-		$template .= '%s<\body>'."\n";
+		$template = '<!DOCTYPE html>' . "\n";
+		$template .= '<html lang="en-EN">' . "\n";
+		$template .= '<head><meta charset="UTF-8"><meta http-equiv="content-type" content="text/html">' .
+			'<meta name="application-name" content="AviLib"><meta name="author" content="Aviato Soft">' .
+			'<meta name="description" content="Web dust library"><meta name="generator" content="AviatoWebBuilder">' .
+			'<meta name="keywords" content="%s">' .
+			'<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+		$template .= '<title>website</title>';
+		$template .= '<link rel="shortcut icon" href="//www.aviato.ro/favicon.ico"/>%s</head>' . "\n";
+		$template .= '<body%s>' . "\n";
+		$template .= '%s' . "\n";
+		$template .= '%s</body>' . "\n";
 		$template .= '</html>';
 
-		$testv = ['', '', '', ''];
+		// test 1: empty default page
+		$testv = [
+			'Aviato, Aviato Soft, Aviato Web',
+			'',
+			'',
+			'',
+			''
+		];
 		$test = vsprintf($template, $testv);
 
-		//var_dump($test);
+		// var_dump($test);
 		ob_start();
 		$aviUi = new AviUi();
-		$aviUi -> Page();
+		$aviUi->Page();
+		$result = ob_get_clean();
+		// var_dump($result); // <-- uncomment this line to see the result!
+		$this->assertEquals($test, $result);
+
+		// test 2: customized head:
+		$testv = [
+			'Test',
+			'<link href="/css/aviato.css" rel="stylesheet" type="ext/css"/><script>;</script>',
+			' class="avi"',
+			'<div>test</div>',
+			'<script src="/js/aviato.js"></script>' . "\n"
+		];
+		$test = vsprintf($template, $testv);
+		// var_dump($test);
+		ob_start();
+		$aviUi = new AviUi();
+		$aviUi->content = [
+			'<div>test</div>'
+		];
+		$aviUi->header = [
+			'<script>;</script>'
+		];
+		$aviUi->page['style'] = [
+			0 => [
+				'href' => '/css/aviato.css'
+			]
+		];
+		$aviUi->page['javascript'] = [
+			0 => [
+				'src' => '/js/aviato.js'
+			]
+		];
+		$aviUi->Page([
+			'class' => 'avi',
+			'meta' => [
+				45 => [
+					'name' => 'keywords',
+					'content' => 'Test'
+				]
+			]
+		]);
 		$result = ob_get_clean();
 
 		// var_dump($result); // <-- uncomment this line to see the result!
 		$this->assertEquals($test, $result);
-
-		ob_start();
-		$testv = [
-			'<link href="/css/aviato.css" rel="stylesheet" type="ext/css"/><script>;</script>',
-			' class="avi"',
-			'<div>test</div>',
-			'<script src="/js/aviato.js"></script>'."\n"
-		];
-		$test = vsprintf($template, $testv);
-		$aviUi = new AviUi();
-		$aviUi -> content = ['<div>test</div>'];
-		$aviUi -> header = ['<script>;</script>'];
-		$aviUi -> page['style'] = [0 => ['href' => '/css/aviato.css']];
-		$aviUi -> page['javascript'] = [0 => ['src' => '/js/aviato.js']];
-		$aviUi -> Page(['class' => 'avi']);
-		$result = ob_get_clean();
-
-		//var_dump($result); // <-- uncomment this line to see the result!
-		$this->assertEquals($test, $result);
-
 	}
 }
