9a56b60ac278dff90d13dfd7fe2b1bd57a6e5aaa
aviato-vasile <vasile@aviato.ro>
2021-04-12 16:59:44 +0300

    update aviato.js

--- src/Version.php
+++ src/Version.php
@@ -5,8 +5,8 @@
  * @author Aviato Soft
  * @copyright Aviato Soft
  * @license GNUv3
- * @version 00.04.02
- * @since  2021-04-08 08:48:41
+ * @version 00.04.03
+ * @since  2021-04-12 16:54:26
  *
  */
 declare(strict_types = 1);
@@ -16,11 +16,11 @@ const AVI_MAJOR = '00';
 
 const AVI_MINOR = '04';
 
-const AVI_PATCH = '02';
+const AVI_PATCH = '03';
 
-const AVI_DATE = '2021-04-08 08:48:41';
+const AVI_DATE = '2021-04-12 16:54:26';
 
-const AVI_JS_MD5 = 'd41208fe05121135fcfafd65be89529f';
+const AVI_JS_MD5 = 'bfd9bd8aeeebaa0a712ce7b72f115800';
 
 /**
  * Version class
