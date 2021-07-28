f24a6dbf9e9ace46a31014f1d713f4e3ac70ad13
aviato-vasile <vasile@aviato.ro>
2021-07-28 13:57:10 +0300

    add Avi\Version::getJsMd5()

--- src/Version.php
+++ src/Version.php
@@ -5,8 +5,8 @@
  * @author Aviato Soft
  * @copyright 2014-present Aviato Soft. All Rights Reserved.
  * @license GNUv3
- * @version 00.06.02
- * @since  2021-07-28 12:32:38
+ * @version 00.06.03
+ * @since  2021-07-28 13:23:08
  *
  */
 declare(strict_types = 1);
@@ -16,9 +16,9 @@ const AVI_MAJOR = '00';
 
 const AVI_MINOR = '06';
 
-const AVI_PATCH = '02';
+const AVI_PATCH = '03';
 
-const AVI_DATE = '2021-07-28 12:32:38';
+const AVI_DATE = '2021-07-28 13:23:08';
 
 const AVI_JS_MD5 = '4195aa75428858c7a06079f785730462';
 
