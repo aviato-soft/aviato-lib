7282ec9b64dfd1ecab6ddbb53919b9761d4b5cdf
Giorgi Vasile <vasile@aviato.ro>
2023-03-28 19:06:55 +0300

    add proxy + test for more easy to use filter get/post/request

--- tests/AviFilter-Test.php
+++ tests/AviFilter-Test.php
@@ -10,6 +10,23 @@ use Avi\Filter as AviFilter;
 final class testAviatoFilter extends TestCase
 {
 
+	private function proxy($query) {
+		$proxyUrl = AVI_TEST_PROXY;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, sprintf("%s?%s",
+			$proxyUrl,
+			http_build_query(array_merge($_GET, ['action' => json_encode($query)])))
+		);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$response = curl_exec($ch);
+
+		curl_close($ch);
+
+		return $response;
+	}
+
 
 	public function testFn_Construct(): void
 	{
@@ -83,8 +100,19 @@ final class testAviatoFilter extends TestCase
 
 	public function testFn_check(): void
 	{
-		$test = $_GET;
-		$test['action'] =$_POST['action'];
+		$_GET = [];
+		$_GET['date'] = '2013-11-09';
+		$_GET['number'] = '22';
+
+		$_POST= [];
+		$_POST['action'] = 'test';
+
+		$test = [
+			'action' => 'test',
+			'date' => '2013-11-09',
+			'number' => '22'
+		];
+
 		$def = [
 			INPUT_GET => [
 				'validate' => [
@@ -139,22 +167,219 @@ final class testAviatoFilter extends TestCase
 		$this->assertEquals($test, $result);
 	}
 
-/*
+
+
+	public function testFn_xssClean(): void
+	{
+		$filter = new AviFilter();
+		$test = '<script\x0Dtype="text/javascript">javascript:alert(1);</script>';
+		$result = $filter->xssClean($test);
+		$this->assertEquals('(1);', $result);
+
+		$test = [
+			'<IMG """><SCRIPT>alert("XSS")</SCRIPT>"\>',
+			'<IMG onmouseover="alert(\'xxs\')">'
+		];
+		$result = $filter->xssClean($test);
+		$this->assertEquals($test, $result);
+	}
+
+
+	public function testFn_post(): void
+	{
+		$filter = new AviFilter();
+		$test = 'Aviato-Soft';
+		$_POST['test'] = $test;
+		$result = $filter->post('test') ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'post',
+			],
+			'args' => [
+				'test'
+			]
+		]);
+		$this->assertEquals($test, $result);
+
+		$test = "<img src=j&#X41vascript:alert('test2')>";
+		$_POST['xss'] = $test;
+		$result = $filter->post('xss') ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'post',
+			],
+			'args' => [
+				'xss'
+			]
+		]);
+		$this->assertEquals('<img >', $result);
+	}
+
+
+	public function testFn_get(): void
+	{
+		$filter = new AviFilter();
+		$test = 'Aviato-Soft';
+		$_GET['test'] = $test;
+		$result = $filter->get('test') ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'get',
+			],
+			'args' => [
+				'test'
+			]
+		]);
+		$this->assertEquals($test, $result);
+	}
+
+
+
+	public function testFn_var(): void
+	{
+		$filter = new AviFilter();
+		$test = 7;
+		$_POST['test'] = $test;
+		$result = $filter->var('test', INPUT_POST, [
+			'number' => 'int',
+			'max' => 10,
+			'min' => 1
+		]) ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'var',
+			],
+			'args' => [
+				'test',
+				INPUT_POST,
+				[
+					'number' => 'int'
+
+				]
+			]
+		]);
+		$this->assertEquals($test, $result);
+
+
+		$test = 7.333;
+		$_POST['test'] = $test;
+		$result = $filter->var('test', INPUT_POST, ['number' => 'float']) ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'var',
+			],
+			'args' => [
+				'test',
+				INPUT_POST,
+				[
+					'number' => 'float'
+
+				]
+			]
+		]);
+		$this->assertEquals($test, $result);
+
+
+		$test = 'Aviato-Soft';
+		$_POST['test'] = $test;
+		$result = $filter->var('test', INPUT_POST, [
+			'id' => FILTER_VALIDATE_REGEXP,
+			'flags' => FILTER_NULL_ON_FAILURE,
+			'options' => [
+				'regexp' => "/^[a-zA-Z0-9]{3,24}$/i"
+			]]) ?? $result = $this->proxy([
+			'call' => [
+				'Filter',
+				'var',
+			],
+			'args' => [
+				'test',
+				INPUT_POST,
+				[
+					'regexp' => "/^[a-zA-Z0-9]{3,24}$/i"
+				]
+			]
+		]);
+		$this->assertFalse($result);
+
+		$result = $filter->var('test', INPUT_POST, [
+			'regexp' => "/^[a-zA-Z0-9_-]{3,24}$/i"
+		]) ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'var',
+			],
+			'args' => [
+				'test',
+				INPUT_POST,
+				[
+					'regexp' => "/^[a-zA-Z0-9_-]{3,24}$/i"
+				]
+			]
+		]);
+		$this->assertEquals($test, $result);
+	}
+
+
 	public function testFn_request(): void
 	{
-		$test = 'Aviato Soft';
-		$filter = new AviFilter([]);
-//		$result = $filter->request('section');
-//		$this->assertEquals('', $result);
+		$filter = new AviFilter();
+		$testG = 'GET: Aviato-Soft';
+		$testP = 'POST: Aviato-Soft';
 
-		$_GET['section'] = $test;
-		$result = $filter->request('section');
-		var_dump($test, $result);
-		//$this->assertEquals($test, $result);
+		//test 1: P[ ] G[ ]
+		$_POST = [];
+		$_GET = [];
+		$result = $filter->request('test') ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'request',
+			],
+			'args' => [
+				'test'
+			]
+		]);
+		$this->assertEquals('', $result);
 
-//		$_POST['section'] = $test;
-//		$result = $filter->request('section');
-//		$this->assertEquals($test, $result);
+		//test2: P[x] G[ ]
+		$_POST['test'] = $testP;
+		$result = $filter->request('test') ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'request',
+			],
+			'args' => [
+				'test'
+			]
+		]);
+		$this->assertEquals($testP, $result);
+
+		//test 3: P[x] G[x]
+		$_GET['test'] = $testG;
+		$result = $filter->request('test') ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'request',
+			],
+			'args' => [
+				'test'
+			]
+		]);
+		$this->assertEquals($testP, $result);
+
+		//test 4: P[ ] G[x]
+		$_POST = [];
+		$result = $filter->request('test') ?? $this->proxy([
+			'call' => [
+				'Filter',
+				'request',
+			],
+			'args' => [
+				'test'
+			]
+		]);
+		$this->assertEquals($testG, $result);
+
 	}
-*/
+
 }
