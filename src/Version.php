af3fafc2ab4c09852272ca9a00edbcaf60fe4444
Giorgi Vasile <vasile@aviato.ro>
2023-04-30 09:45:26 +0300

    new version 1.23.15

--- src/Version.php
+++ src/Version.php
@@ -5,8 +5,8 @@
  * @author Aviato Soft
  * @copyright 2014-present Aviato Soft. All Rights Reserved.
  * @license GNUv3
- * @version 01.23.14
- * @since  2023-03-28 21:16:18
+ * @version 01.23.15
+ * @since  2023-04-30 09:36:34
  *
  */
 declare(strict_types = 1);
@@ -16,9 +16,9 @@ const AVI_MAJOR = '01';
 
 const AVI_MINOR = '23';
 
-const AVI_PATCH = '14';
+const AVI_PATCH = '15';
 
-const AVI_DATE = '2023-03-28 21:16:18';
+const AVI_DATE = '2023-04-30 09:36:34';
 
 const AVI_JS_MD5 = 'c1fe4ad9c558fb59f5d7e20cb1f5ba46';
 
