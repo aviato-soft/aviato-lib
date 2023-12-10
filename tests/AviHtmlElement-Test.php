c04be9ec1db5c10eff247d0667d53e5ff85a492e
Vasile Giorgi <vasile@aviato.ro>
2023-12-10 11:09:53 +0200

    test for HtmlElements

--- tests/AviHtmlElement-Test.php
+++ tests/AviHtmlElement-Test.php
@@ -116,4 +116,112 @@ final class testAviatoHtmlElement extends TestCase
 		$test = '<body><button class="btn btn-primary" type="submit">click me!</button></body>';
 		$this->assertEquals($test, $result);
 	}
+
+
+	public function testFn_HtmlElementBsButton()
+	{
+		//button base class
+		$test = '<button class="btn" type="button"></button>';
+		$aviHtmlElement = new AviHtmlElement();
+		$result = $aviHtmlElement->element('BsButton', [])->use();
+		$this->assertEquals($test, $result);
+
+		//variants
+		$test = '<button class="btn btn-dark" type="button"></button>';
+		$result = $aviHtmlElement->element('BsButton', [
+				'variant' => 'dark'
+			])->use();
+		$this->assertEquals($test, $result);
+
+		//disable text wrapping
+		$test = '<button class="btn btn-primary text-nowrap" type="button"></button>';
+		$result = $aviHtmlElement->element('BsButton', [
+			'variant' => 'primary',
+			'nowrap' => true
+		])->use();
+		$this->assertEquals($test, $result);
+
+
+		//multiple attributes
+		$test = '<button aria-pressed="true" class="active btn btn-outline-dark btn-sm text-nowrap" data-bs-toggle="button" disabled type="button">Button</button>';
+		$result = $aviHtmlElement->element('BsButton', [
+			'active' => true,
+			'disabled' => true,
+			'nowrap' => true,
+			'outline' => true,
+			'size' => 'sm',
+			'text' => 'Button',
+			'variant' => 'dark',
+		])->use();
+		$this->assertEquals($test, $result);
+
+		//buton type link
+		$test = '<a aria-disabled="true" class="btn disabled" role="button" tab-index="-1">Click me!</a>';
+		$result = $aviHtmlElement->element('BsButton', [
+			'disabled' => true,
+			'tag' => 'a',
+			'text' => 'Click me!'
+		])->use();
+		$this->assertEquals($test, $result);
+
+
+		//buton type input
+		$test = '<input class="btn btn-secondary" disabled type="reset" value="Click me!">';
+		$result = $aviHtmlElement->element('BsButton', [
+			'disabled' => true,
+			'tag' => 'input',
+			'text' => 'Click me!',
+			'type' => 'reset',
+			'variant' => 'secondary'
+		])->use();
+		$this->assertEquals($test, $result);
+
+
+		//buton type submit
+		$test = '<button class="btn btn-secondary" disabled type="submit">Click me!</button>';
+		$result = $aviHtmlElement->element('BsButton', [
+			'disabled' => true,
+			'text' => 'Click me!',
+			'type' => 'submit',
+			'variant' => 'secondary'
+		])->use();
+		$this->assertEquals($test, $result);
+
+		//icon base class
+		$test = '<i class="bi bi-airplane"></i>';
+		$result = $aviHtmlElement->element('BsIcon', ['airplane'])->use();
+		$this->assertEquals($test, $result);
+
+		//button with icon
+		$test = '<button class="btn" type="button"><i class="bi bi-airplane"></i><span class="ps-2">Click me!</span></button>';
+		$result = $aviHtmlElement->element('BsButton', [
+			'icon' => 'airplane',
+			'text' => 'Click me!'
+		])->use();
+		$this->assertEquals($test, $result);
+
+		$result = $aviHtmlElement->element('BsButton', [
+			'icon' => ['airplane'],
+			'text' => 'Click me!'
+		])->use();
+		$this->assertEquals($test, $result);
+
+		$result = $aviHtmlElement->element('BsButton', [
+			'icon' => $aviHtmlElement->element('BsIcon', ['airplane'])->use(),
+			'text' => 'Click me!'
+		])->use();
+		$this->assertEquals($test, $result);
+
+		$test = implode('', [
+			'<button class="btn btn-sm btn-primary" ',
+			'data-action="widget" data-widget="Gear" data-success="backend.on.success.done" data-serialize="true" data-call="Edit" data-gear="hotel" data-verbose="true" ',
+			'type="button">',
+			'<i class="bi bi-floppy-fill" data-role="btn-icon"></i>',
+			'<span class="spinner-border spinner-border-sm d-none" data-role="spinner" role="status" aria-hidden="true"></span>',
+			'<span class="visually-hidden">Please wait...</span>',
+			'<span class="ps-2">Save</span></button>'
+		]);
+
+	}
+
 }
