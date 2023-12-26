64396eeccae2148aaf47f4e8a4b8bda58713f15f
Vasile Giorgi <vasile@aviato.ro>
2023-12-26 18:53:39 +0200

    new html element nav

---
+++ src/HtmlElement/HtmlElementBsNavItem.php
@@ -0,0 +1,105 @@
+<?php
+/*
+ * License
+ *
+ * @author Aviato Soft
+ * @copyright 2014-present Aviato Soft. All Rights Reserved.
+ * @license GNUv3
+ * @version 01.23.24
+ * @since  2023-12-15 18:03:04
+ *
+ */
+declare(strict_types = 1);
+namespace Avi;
+
+require_once dirname(__DIR__).'/HtmlElement.php';
+require_once __DIR__.'/HtmlElementBs.php';
+
+class HtmlElementBsNavItem extends HtmlElement
+{
+	private $position;
+
+	protected $params;
+
+	public function __construct($params = [])
+	{
+		$this->params = $params;
+		$this->parseParams();
+		$this->setAttributes();
+		$this->setContent();
+
+		return $this;
+	}
+
+
+	private function parseParams()
+	{
+		$this->parseParam('text');
+		$this->tag = 'a';
+
+		$this->parseParam('active', false);
+		$this->parseParam('disabled', false);
+		$this->parseParam('href', 'javascript:;');
+	}
+
+
+	private function setAttributes()
+	{
+		$this->setAttributeHref();
+		$this->setAttributeActive();
+		$this->setAttributeDisabled();
+
+		$this->attributes([
+			'class' => [
+				'nav-link'
+			]
+		]);
+	}
+
+
+	private function setAttributeActive()
+	{
+		if ($this->params['active']) {
+			$this->attributes([
+				'aria' => [
+					'current' => 'page'
+				],
+				'class' => [
+					'active'
+				]
+			]);
+		}
+	}
+
+
+	private function setAttributeDisabled()
+	{
+		if ($this->params['disabled'] === true) {
+			$this->attributes([
+				'class' => 'disabled',
+				'aria' => [
+					'disabled' => 'true'
+				],
+			]);
+			if(isset($this->attributes['href'])) {
+				$this->attributes['tabindex'] = '-1';
+			}
+		}
+	}
+
+
+	private function setAttributeHref()
+	{
+		if ($this->params['href'] !== false) {
+			$this->attributes([
+				'href' => $this->params['href']
+			]);
+		}
+	}
+
+
+	private function setContent()
+	{
+		$this->content = $this->params['text'];
+	}
+}
