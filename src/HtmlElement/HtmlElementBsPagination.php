5af0aa863d427fc8913101ba90d1ddaf5a00eaab
Vasile Giorgi <vasile@aviato.ro>
2024-02-07 10:43:11 +0200

    new version: 1.24.00

--- src/HtmlElement/HtmlElementBsPagination.php
+++ src/HtmlElement/HtmlElementBsPagination.php
@@ -5,8 +5,8 @@
  * @author Aviato Soft
  * @copyright 2014-present Aviato Soft. All Rights Reserved.
  * @license GNUv3
- * @version 01.23.25
- * @since  2023-12-27 12:33:50
+ * @version 01.24.00
+ * @since  2024-02-06 21:30:40
  *
  */
 declare(strict_types = 1);
