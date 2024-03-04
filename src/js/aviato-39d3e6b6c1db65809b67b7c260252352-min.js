a917dcbd49cb361f14130478708182df8db59837
Vasile Giorgi <vasile@aviato.ro>
2024-03-04 18:28:05 +0200

    version 01.24.09

--- src/js/aviato-39d3e6b6c1db65809b67b7c260252352-min.js
+++ src/js/aviato-39d3e6b6c1db65809b67b7c260252352-min.js
@@ -1,5 +1,5 @@
 /* 
-Aviato-Lib.js, build #01.24.08 from 2024-02-24 13:27:06.
+Aviato-Lib.js, build #01.24.09 from 2024-03-04 12:25:06.
 Copyright 2014-present Aviato Soft. All Rights Reserved.
  */"use strict";function typeOf(value){var s=typeof value;if(s==='object'){if(value){if(value instanceof Array){s='array';}}else{s='null';}}
 return s;}
